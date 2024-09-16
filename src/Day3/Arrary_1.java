77d90c4d0a16a02ab4d38388ffdf4842497e2c3f
Pont26 <nyeinkhinkhin.tgi56@gmail.com>
2024-09-16 22:22:58 +0700

    add something

--- src/Day3/Arrary_1.java
+++ src/Day3/Arrary_1.java
@@ -2,7 +2,7 @@ package Day3;
 
 public class Arrary_1 {
     public static void main(String[] args){
-      //  int[] number=new int[5];
+       int[] number=new int[5];
         int[] number={1,2,3,3,6};
 
          for(int i=0;i<number.length;i++){
