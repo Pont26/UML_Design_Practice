6bf5237677dfb9ba20db4c9a124af8623a276b42
Pont26 <nyeinkhinkhin.tgi56@gmail.com>
2024-09-16 22:02:17 +0700

    type something

--- src/Day_2/Looping_1.java
+++ src/Day_2/Looping_1.java
@@ -3,7 +3,7 @@ package Day_2;
 public class Looping_1 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-
+//something
             System.out.println(i +" * 2= "+ (i*2));
         }
     }
