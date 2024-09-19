b17b70b4f21af1a404a030f751e0fe88dad3c508
Min Pyae Hein <minpyaehein536h@prestigein.com>
2024-09-19 22:34:21 +0700

    add some output in lopping

--- src/Day_2/Looping_1.java
+++ src/Day_2/Looping_1.java
@@ -5,6 +5,10 @@ public class Looping_1 {
         for (int i = 1; i <= 10; i++) {
 //something
             System.out.println(i +" * 2= "+ (i*2));
+            System.out.println(i +" * 3= "+ (i*2));
+            System.out.println(i +" * 5= "+ (i*2));
+            System.out.println(i +" * 6= "+ (i*2));
+            System.out.println(i +" * 2= "+ (i*2));
         }
     }
 }
